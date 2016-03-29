aa2e0a34638c5c71d4e47da71428d6bf675f3261
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-29 22:51:55 +0100

    down button working too. yay

--- interface.js
+++ interface.js
@@ -1,11 +1,16 @@
 $(document).ready(function () {
   var thermostat = new Thermostat();
 
-  $("#temp").prepend(thermostat.temperature);
+  $("#temp").prepend(thermostat.showTemperature());
 
   $("#temp-up").click(function () {
     thermostat.up();
-    $("#temp").text(thermostat.temperature);
-  })
+    $("#temp").text(thermostat.showTemperature());
+  });
+
+  $("#temp-down").click(function () {
+    thermostat.down();
+    $("#temp").text(thermostat.showTemperature());
+  });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -6,12 +6,12 @@ function Thermostat() {
   this.MAX_TEMP_PSM_ON = 25;
   this.MAX_TEMP_PSM_OFF = 32
   this.MEDIUM_USAGE_LIMIT = 18;
-  this.temperature = this.DEFAULT_TEMP;
+  this._temperature = this.DEFAULT_TEMP;
   this._powerSaving = true;
 };
 
 Thermostat.prototype.showTemperature = function () {
-  return this.temperature;
+  return this._temperature;
 };
 
 Thermostat.prototype.up = function () {
@@ -19,7 +19,7 @@ Thermostat.prototype.up = function () {
     return;
   }
   else {
-    this.temperature += 1;
+    this._temperature += 1;
   }
 };
 
@@ -28,24 +28,24 @@ Thermostat.prototype.down = function () {
     return;
   }
   else {
-    this.temperature -= 1;
+    this._temperature -= 1;
   }
 };
 
 Thermostat.prototype.resetTemperature = function () {
-  this.temperature = this.DEFAULT_TEMP;
+  this._temperature = this.DEFAULT_TEMP;
 };
 
 Thermostat.prototype.isMinTemperature = function () {
-  return this.temperature === this.MIN_TEMP;
+  return this._temperature === this.MIN_TEMP;
 };
 
 Thermostat.prototype.isMaxTemperature = function () {
   if (this.isPowerSaving()) {
-    return this.temperature === this.MAX_TEMP_PSM_ON;
+    return this._temperature === this.MAX_TEMP_PSM_ON;
   }
   else {
-    return this.temperature === this.MAX_TEMP_PSM_OFF;
+    return this._temperature === this.MAX_TEMP_PSM_OFF;
   }
 };
 
