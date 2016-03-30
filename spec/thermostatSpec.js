27433a16f39dc367af3b0b3551e9074a4d5ac43a
Sachin Karia <sachin.karia01@gmail.com>
2016-03-30 13:29:16 +0100

    added decrease/increase temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,8 +7,19 @@ describe('Thermostat', function() {
   });
 
   describe('#temperature', function() {
+
     it('default temperature 20', function() {
-      expect(thermostat.temperature).toEqual(20);
+      expect(thermostat.currentTemperature()).toEqual(20);
     });
+
+    it('temperature up increase temperature by 1', function(){
+      thermostat.increaseTemperature()
+      expect(thermostat.currentTemperature()).toEqual(21);
+    })
+
+    it('thermostat down decrease temperature by 1', function(){
+      thermostat.decreaseTemperature()
+      expect(thermostat.currentTemperature()).toEqual(19);
+    })
 });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,3 +1,15 @@
 function Thermostat(){
   this.temperature = 20;
 }
+
+Thermostat.prototype.currentTemperature = function(){
+  return this.temperature
+}
+
+Thermostat.prototype.increaseTemperature = function(){
+  this.temperature ++ ;
+}
+
+Thermostat.prototype.decreaseTemperature = function(){
+  this.temperature -- ;
+}
