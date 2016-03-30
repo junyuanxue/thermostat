98c9d0addb04840caff9809803173c1f6ccdeb32
eripheebs <erikapheby@gmail.com>
2016-03-30 12:03:42 +0100

    Thermostat clicking

---
+++ interface.js
@@ -0,0 +1,40 @@
+$ ( document ).ready(function () {
+  thermostat = new Thermostat()
+
+  $ ( "#temperature" ).text(thermostat.temperature);
+
+  $ ( "#temperature" ).addClass(thermostat.energyUsage());
+
+  $ ( "#psm" ).text("On");
+
+  $ ( "#increase" ).click(function () {
+    thermostat.increase();
+    updateTemperature();
+  });
+
+  $ ( "#decrease" ).click(function () {
+    thermostat.decrease();
+    updateTemperature();
+  });
+
+  $ ( "#reset" ).click(function () {
+    thermostat.resetTemp();
+    updateTemperature();
+  });
+
+  $ ( "#psm-on" ).click(function () {
+    thermostat.powerSavingOn();
+    $ ( "#psm" ).text("On");
+  });
+
+  $ ( "#psm-off" ).click(function () {
+    thermostat.powerSavingOff();
+    $ ( "#psm" ).text("Off");
+  });
+
+  updateTemperature = function() {
+    $ ( "#temperature" ).text(thermostat.temperature);
+    $ ( "#temperature" ).addClass(thermostat.energyUsage());
+  };
+
+});
